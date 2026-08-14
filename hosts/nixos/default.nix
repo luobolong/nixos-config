1680bda974f83424ea09f41c4ef524bd9a4df8a0
luobolong <benjcarrot@gmail.com>
2026-08-14 14:54:54 +0800

    diable /modules/secrets.nix

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -6,7 +6,7 @@
     ../../modules/core.nix
     ../../modules/desktop.nix
     ../../modules/refind.nix
-    ../../modules/secrets.nix
+    # ../../modules/secrets.nix
     ../../modules/snapper.nix
   ];
 
